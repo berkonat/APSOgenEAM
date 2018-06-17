d52804458360de1b3846f423dbc6e4a61d5fe959
berkonat <b.onat@warwick.ac.uk>
2018-06-17 21:09:38 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 source /progs/intel/impi/4.0.3.008/bin64/mpivars.sh
 source /progs/intel/composerxe/bin/compilervars.sh intel64
 source /progs/intel/composerxe/mkl/bin/mklvars.sh intel64
-make -f Makefile.parallel clean
-make -f Makefile.parallel
+make clean
+make
